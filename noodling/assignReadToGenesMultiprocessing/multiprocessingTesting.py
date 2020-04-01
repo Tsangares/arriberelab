655a2a54c746cd1143f6eb4de9300f56e70b8541
William Wyatt <tsangares@gmail.com>
2020-04-01 14:11:22 -0700

    Added example of multiprocessing

--- noodling/assignReadToGenesMultiprocessing/multiprocessingTesting.py
+++ noodling/assignReadToGenesMultiprocessing/multiprocessingTesting.py
@@ -11,17 +11,29 @@ Major test will be to compare the speeds of writing a bunch of csv lines to a fi
 
 Main plan will be to implement a toy method for each of the above then run it at a very high repitition
 """
-
+from multiprocessing import Pool 
+from itertools import product
+"""
 import pandas as pd
 import numpy as np
 
 # Imports from assignReadsToGenes4.py >>>
 import sys, os, collections, csv, common, re, time, pickle, copy, linecache
-import pandas
+#import pandas
 from logJosh import Tee
+
 csv.field_size_limit(sys.maxsize)
 # <<< Imports from assignReadsToGenes4.py
-
+"""
+#inputs is an array of tuples
+def single_process(number,power):
+    return number**power
+def wil_multi(inputNumbers,inputPowers,CPU_CORES=4):
+    with Pool(CPU_CORES) as p:
+        results = p.starmap(single_process,zip(inputNumbers,inputPowers))
+        
+    for number,power,result in zip(inputNumbers,inputPowers,results):
+        print(f"Taking {number} to the power of {power} is {result}")
 
 def multiprocessing_with_csv():
     def single_process_to_csv():
@@ -44,4 +56,7 @@ def main(number_of_repititions: int):
 
 
 if __name__ == '__main__':
-    main(1)
+    #main(1)
+    inputNumbers = [i for i in range(100)]
+    inputPowers = [i for i in range(100)]
+    wil_multi(inputNumbers,inputPowers)
