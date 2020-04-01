568e005e1561869641ffd725777c4dfaf2dbc0c3
William Wyatt <tsangares@gmail.com>
2020-04-01 15:03:32 -0700

    Added writing queue

--- noodling/assignReadToGenesMultiprocessing/multiprocessingTesting.py
+++ noodling/assignReadToGenesMultiprocessing/multiprocessingTesting.py
@@ -11,8 +11,9 @@ Major test will be to compare the speeds of writing a bunch of csv lines to a fi
 
 Main plan will be to implement a toy method for each of the above then run it at a very high repitition
 """
-from multiprocessing import Pool 
-from itertools import product
+from multiprocessing import Pool,Queue,Process
+from itertools import product,repeat
+import random,time
 """
 import pandas as pd
 import numpy as np
@@ -26,14 +27,50 @@ csv.field_size_limit(sys.maxsize)
 # <<< Imports from assignReadsToGenes4.py
 """
 #inputs is an array of tuples
-def single_process(number,power):
-    return number**power
-def wil_multi(inputNumbers,inputPowers,CPU_CORES=4):
-    with Pool(CPU_CORES) as p:
-        results = p.starmap(single_process,zip(inputNumbers,inputPowers))
+outputFile = 'log.log.log'
+queue = Queue()
+def writing_process(total,BATCH_SIZE=10):
+    writtenElements = 0
+    memory = {} #unorderd
+    output = [] #ordered
+    while writtenElements != total:
+        if queue.qsize() != 0:
+            index,value = queue.get()
+            if len(output) == index:
+                output.append(str(value))
+            else:
+                memory[index] = str(value)
+                
+        try: #Try to add to the output in an ordered way.
+            output.append(memory[len(output)])
+            print(f"Appened {memory[len(output)]}",len(output))
+        except KeyError:
+            pass
         
-    for number,power,result in zip(inputNumbers,inputPowers,results):
-        print(f"Taking {number} to the power of {power} is {result}")
+        #write if I have a batch size of elements.
+        if int(len(output)/BATCH_SIZE) > int(writtenElements/BATCH_SIZE) or total-writtenElements<BATCH_SIZE:
+            elementsToWrite = output[writtenElements:]
+            with open(outputFile,'a') as f:
+                f.write('\n'.join(elementsToWrite))
+                f.write('\n')
+            writtenElements += len(elementsToWrite)
+    
+def single_process(index,number,power):
+    for i in range(10**5):
+        value = number**power
+    queue.put((index,value))
+    return value
+    
+def wil_multi(indexes,inputNumbers,power,CPU_CORES=4):
+    writer = Process(target=writing_process,args=(len(indexes),))
+    writer.start()
+    with Pool(CPU_CORES) as p:
+        results = p.starmap(single_process,zip(indexes,inputNumbers,repeat(power)))
+        
+    for number,power,result in zip(inputNumbers,repeat(inputPower),results):
+        #print(f"Taking {number} to the power of {power} is {result}")
+        pass
+    
 
 def multiprocessing_with_csv():
     def single_process_to_csv():
@@ -57,6 +94,13 @@ def main(number_of_repititions: int):
 
 if __name__ == '__main__':
     #main(1)
-    inputNumbers = [i for i in range(100)]
-    inputPowers = [i for i in range(100)]
-    wil_multi(inputNumbers,inputPowers)
+    SIZE = 100
+    
+    #rewrite output file
+    with open(outputFile,'w+') as f:
+        f.write("")
+
+    indexes = [i for i in range(SIZE)]
+    inputNumbers = [i for i in range(SIZE)]
+    inputPower = 2
+    wil_multi(indexes,inputNumbers,inputPower)
